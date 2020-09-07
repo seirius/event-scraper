b68264debd78712eeb00b2f461cf9ed87bc39947
Andriy <andsei91@gmail.com>
2020-09-07 21:50:01 +0200

    update: first version ticket master

--- src/search/TicketmasterService.ts
+++ src/search/TicketmasterService.ts
@@ -1,50 +1,44 @@
-import axios from "axios";
 import * as cheerio from "cheerio";
 import { Injectable } from "@nestjs/common";
 import { WebTargetConfig } from "src/config/WebTargetConfig";
 import { ScrappedEvent } from "./helpers/ScrappedEvent";
+import { LaunchOptions } from "puppeteer";
 import puppeteerExtra from "puppeteer-extra";
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import { promises } from "fs";
+import AdBlockPlugin from "puppeteer-extra-plugin-adblocker";
+import { EventHouse } from "./helpers/EventHouse";
 puppeteerExtra.use(StealthPlugin());
+puppeteerExtra.use(AdBlockPlugin());
 
-const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-infobars',
-    '--window-position=0,0',
-    '--ignore-certifcate-errors',
-    '--ignore-certifcate-errors-spki-list',
-    `--user-agent="${WebTargetConfig.TICKETMASTER_HEADER_USER_AGENT}"`
-];
-
-const options = {
-    args,
-    headless: true
+const options: LaunchOptions = {
+    headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '-disable-gpu', '--no-first-run', '--disable-notifications'],
+    ignoreDefaultArgs: ["--enable-automation"]
 };
 
 @Injectable()
 export class TicketmasterService {
     private static readonly API_PATH = "/search";
-    private static readonly RADIUS_PARAM_NAME = "radius";
-    private static readonly RADIUS_PARAM = 10000;
-    private static readonly SORT_PARAM_NAME = "sort";
-    private static readonly SORT_PARM = "relevance%2Cdesc";
     private static readonly TAB_PARAM_NAME = "tab";
     private static readonly TAB_PARAM = "events";
     private static readonly Q_PARAM_NAME = "q";
-    private static readonly DATE_RANGE_PARAM_NAME = "daterange";
-    private static readonly DATE_RANGE_PARAM = "all";
 
     private static readonly TM_EVENT_TAB = "#tab--2";
     private static readonly TM_EVENT_ITEM = ".accordion__item.event-listing__item";
 
+    private static readonly CLASS_TITLE = ".event-tile__title";
+    private static readonly CLASS_IMG = ".event-listing__thumbnail";
+    private static readonly CLASS_ADDRESS = ".event-tile__sub-title";
+    private static readonly CLASS_URL = ".event-listing__item-cta";
+
+    private static readonly ATTR_SRC = "src";
+    private static readonly ATTR_HREF = "href";
+
+    private static readonly TIMEOUT_VALUE = 5000;
+
     public getTicketmasterQueryUrl(query: string): string {
         const url = new URL(TicketmasterService.API_PATH, WebTargetConfig.TICKETMASTER_URL);
-        url.searchParams.append(TicketmasterService.RADIUS_PARAM_NAME, TicketmasterService.RADIUS_PARAM.toString());
-        url.searchParams.append(TicketmasterService.SORT_PARAM_NAME, TicketmasterService.SORT_PARM);
         url.searchParams.append(TicketmasterService.TAB_PARAM_NAME, TicketmasterService.TAB_PARAM);
-        url.searchParams.append(TicketmasterService.DATE_RANGE_PARAM_NAME, TicketmasterService.DATE_RANGE_PARAM);
         url.searchParams.append(TicketmasterService.Q_PARAM_NAME, query);
         return url.toString();
     }
@@ -52,30 +46,39 @@ export class TicketmasterService {
     public async getTicketmasterEvents(query: string): Promise<ScrappedEvent[]> {
         const browser = await puppeteerExtra.launch(options);
         const page = await browser.newPage();
-        await page.setRequestInterception(true);
-        page.on('request', request => {
-            request.continue({
-                headers: {
-                    "user-agent": WebTargetConfig.TICKETMASTER_HEADER_USER_AGENT
-                }
-            });
-        });
+        await page.setUserAgent(WebTargetConfig.TICKETMASTER_HEADER_USER_AGENT);
         await page.goto(this.getTicketmasterQueryUrl(query));
-        await page.waitFor(2000);
+        await page.setJavaScriptEnabled(true);
+        await page.setCacheEnabled(true);
+        await page.waitForSelector(TicketmasterService.TM_EVENT_TAB, {
+            timeout: TicketmasterService.TIMEOUT_VALUE
+        });
         const pageHtml = await page.content();
-        // const response = await axios(this.getTicketmasterQueryUrl(query), {
-        //     headers: {
-        //         "user-agent": WebTargetConfig.TICKETMASTER_HEADER_USER_AGENT
-        //     }
-        // });
         await browser.close();
         const $ = cheerio.load(pageHtml);
-        await promises.writeFile("/home/andriy/text.html", pageHtml);
         const $tab = $(TicketmasterService.TM_EVENT_TAB);
         const $events = $($tab[0]).find(TicketmasterService.TM_EVENT_ITEM);
+        const events: ScrappedEvent[] = [];
         $events.each((index, element) => {
-            console.log(element);
+            const $element = $(element);
+            const title = $($element.find(TicketmasterService.CLASS_TITLE).get(0)).text();
+            const img = $element.find(TicketmasterService.CLASS_IMG).attr(TicketmasterService.ATTR_SRC);
+            const address = $element.find(TicketmasterService.CLASS_ADDRESS).text();
+            const url = $element.find(TicketmasterService.CLASS_URL).attr(TicketmasterService.ATTR_HREF);
+            const imgObject = new URL(img);
+            events.push({
+                type: EventHouse.TICKETMASTER,
+                title,
+                img: imgObject.origin + imgObject.pathname,
+                url,
+                instances: [
+                    {
+                        address,
+                        dates: []
+                    }
+                ]
+            });
         });
-        return [];
+        return events;
     }
 }
